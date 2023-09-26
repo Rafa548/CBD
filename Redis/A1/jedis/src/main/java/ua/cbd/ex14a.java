3b535c5ed15042d85b882378cc345045fdfc9b25
Rafa548 <rafael3vilaca30@gmail.com>
2023-09-26 17:01:18 +0100

    txt adicionados e codigo limpo

--- Redis/A1/jedis/src/main/java/ua/cbd/ex14a.java
+++ Redis/A1/jedis/src/main/java/ua/cbd/ex14a.java
@@ -2,8 +2,6 @@ package ua.cbd;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 import redis.clients.jedis.Jedis;
@@ -15,13 +13,13 @@ public class ex14a {
         Jedis jedis = new Jedis();
 
         File file = new File("Redis/A1/names.txt");
-        Scanner sc_in = new Scanner(System.in);  // Create a Scanner object
+        Scanner sc_in = new Scanner(System.in); 
 
         try {
             Scanner myReader = new Scanner(file);
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
-                jedis.sadd("names",data);
+                jedis.sadd("autocomplete_a",data);
 
             }
         myReader.close();
@@ -36,7 +34,7 @@ public class ex14a {
         System.out.print("Search for ('Enter' to quit): ");
         String search = sc_in.nextLine(); 
         while (!search.equals("")){
-                List<String> autocompleted = jedis.sort("names", sortingParams);;
+                List<String> autocompleted = jedis.sort("autocomplete_a", sortingParams);;
                 for (String name : autocompleted){
                     if (name.startsWith(search)){
                         System.out.println(name);
--- Redis/A1/jedis/src/main/java/ua/cbd/ex14b.java
+++ Redis/A1/jedis/src/main/java/ua/cbd/ex14b.java
@@ -1,27 +1,20 @@
 package ua.cbd;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.Scanner;
-import java.util.Set;
-
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvValidationException;
-
 import java.io.FileReader;
 import java.io.IOException;
-
 import redis.clients.jedis.Jedis;
-import redis.clients.jedis.params.SortingParams;
 
 public class ex14b {
 
     public static void main(String[] args) throws CsvValidationException {
-        //read from file 
+
         Jedis jedis = new Jedis();
-        String csvFilePath = "Redis/A1/nomes-pt-2021.csv"; // Replace with your CSV file path
-        Scanner sc_in = new Scanner(System.in);  // Create a Scanner object
+        String csvFilePath = "Redis/A1/nomes-pt-2021.csv"; 
+        Scanner sc_in = new Scanner(System.in); 
 
         
         try (CSVReader reader = new CSVReader(new FileReader(csvFilePath))) {
@@ -30,7 +23,7 @@ public class ex14b {
                     String column1 = line[0];
                     String name = column1.split(";")[0];
                     Double numb = Double.parseDouble(column1.split(";")[1]);
-                    jedis.zadd("names", numb, name);
+                    jedis.zadd("autocomplete_b", numb, name);
                 }
             
         } catch (IOException e) {
@@ -41,7 +34,7 @@ public class ex14b {
         System.out.print("Search for ('Enter' to quit): ");
         String search = sc_in.nextLine(); 
         while (!search.equals("")){
-                List<String> sortedSetValues = jedis.zrevrange("names", 0, -1);  //zrevrange decrescente zrange crescente
+                List<String> sortedSetValues = jedis.zrevrange("autocomplete_b", 0, -1);  //zrevrange decrescente zrange crescente
                 for (String name : sortedSetValues){
                     if (name.startsWith(search)){
                         System.out.println(name);
